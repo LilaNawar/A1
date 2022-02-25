43a669bf2a91c61319e6a7c1eacc1e6740fe7844
LilaNawar <76576058+LilaNawar@users.noreply.github.com>
2022-02-25 18:19:27 -0500

    first set of files was not correct. Upload again

--- script.js
+++ script.js
@@ -1,25 +1,43 @@
 let input =1;
-let result = 0;
-let resultNum = 0
-
-
-
-
-
+let resultNum = 0;
+let final = document.getElementById("result").innerText;
 
 function addNum(){
-  resultNum=document.getElementById("result");
-  resultNum.innerText = result +10
-
+    let input = document.getElementById("field").value
+    final = parseInt(final)
+    input = parseInt(input)
+    if (final=>0){
+      document.getElementById("result").innerText= parseInt(document.getElementById("result").innerText) + 
+      parseInt(document.getElementById("field").value)
+        if(document.getElementById("result").innerText >=0){
+          document.getElementById("result").style.color = "black";
+        }
+        else{
+          document.getElementById("result").style.color = "red";
+        }
+     }
+      else {
+        console.log(final)
+        final = (document.getElementById("result").innerText + 1);
+  }
 }
+function subNum(){    
+   let input = document.getElementById("field").value
+   final = parseInt(final)
+   input = parseInt(input)
+    if(final>=0){
+      document.getElementById("result").innerText= parseInt(document.getElementById("result").innerText) - 
+    parseInt(document.getElementById("field").value)
+      
+      if(document.getElementById("result").innerText >=0){
+        document.getElementById("result").style.color = "black";
+      }
+      else{
+        document.getElementById("result").style.color = "red";
+      }
+    }
+   else{
+      final = (document.getElementById("result").innerText - 1)
+  }
 
-function doTheMath(){
-  //input = document.getElementById("enternumber").innerText;
-  console.log(1)
-  
-}
-
-
-function subNum(){
-  //document.getelementbyId()
 }
