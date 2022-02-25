989f759cb62baab6f8c22d16069af25b497fcf39
LilaNawar <76576058+LilaNawar@users.noreply.github.com>
2022-02-25 18:17:41 -0500

    Assignment 1 complete with Bonus

---
+++ script.js
@@ -0,0 +1,25 @@
+let input =1;
+let result = 0;
+let resultNum = 0
+
+
+
+
+
+
+function addNum(){
+  resultNum=document.getElementById("result");
+  resultNum.innerText = result +10
+
+}
+
+function doTheMath(){
+  //input = document.getElementById("enternumber").innerText;
+  console.log(1)
+  
+}
+
+
+function subNum(){
+  //document.getelementbyId()
+}
